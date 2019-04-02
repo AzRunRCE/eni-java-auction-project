73b3de628059b64aef36cd0d15558ff3e85df4b6
Administrateur <Administrateur@27-UC37-13.ad.campus-eni.fr>
2019-04-02 10:33:54 +0200

    fix naming

--- Encheres/src/fr/eni/ecole/DAL/DAOFactory.java
+++ Encheres/src/fr/eni/ecole/DAL/DAOFactory.java
@@ -3,8 +3,8 @@ package fr.eni.ecole.DAL;
 import fr.eni.ecole.beans.Utilisateur;
 import fr.eni.ecole.util.AccesBase;
 
-public class Factory {
-	public static DAO<Utilisateur> getUlitidateurDAO() throws DALException{
+public class DAOFactory {
+	public static DAO<Utilisateur> getUtilisateurDAO() throws DALException{
 		return new UtilisateurDAO(AccesBase.getConnection());
 	}
 }

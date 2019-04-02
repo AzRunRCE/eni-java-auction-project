379fd9a871e1c5f4cf952be40b013a201c6871e3
romai <romai@DESKTOP-PVOFP55>
2019-04-02 09:42:47 +0200

    refacto pages et routes

--- Encheres/src/fr/eni/ecole/servlet/AccueilServlet.java
+++ Encheres/src/fr/eni/ecole/servlet/AccueilServlet.java
@@ -2,8 +2,7 @@ package fr.eni.ecole.servlet;
 
 import java.io.IOException;
 
-
-import javax.servlet.Servlet;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,30 +10,37 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class Default
+ * Servlet implementation class AccueilServlet
  */
-@WebServlet("/Default")
-public class Default extends HttpServlet implements Servlet {
+@WebServlet( urlPatterns = {
+		"/",
+		"/Accueil"
+})
+public class AccueilServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Default() {
+    public AccueilServlet() {
         super();
+        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		this.getServletContext().getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
+		// TODO Auto-generated method stub
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/pages/accueil.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
